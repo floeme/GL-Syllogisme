60c4df68411344a2a51ed814d8f6b20256014910
Axel Leblanc <59664317+axllblc@users.noreply.github.com>
2024-12-02 18:07:44 +0100

    Improved figure selector

--- syllogismator/src/components/elements/syllogism/basic/SyllogismFigures.tsx
+++ syllogismator/src/components/elements/syllogism/basic/SyllogismFigures.tsx
@@ -1,62 +1,47 @@
-import { Figure } from "../../../../model/Figure"
+import {Figure} from "../../../../model/Figure"
 
 interface SyllogismFigureProps {
     figure: Figure
     setFigure: (value: Figure) => void
 }
 
-function SyllogismTerms({ figure, setFigure }: SyllogismFigureProps) {
+function SyllogismTerms({figure, setFigure}: SyllogismFigureProps) {
     return (
         <div className="section-figures">
-            {/* FIGURE 1 */}
-            <div className="figure">
-                <label>Figure 1</label>
-                <img src="/images/figure1.png" alt="Figure 1" />
-                <input type="radio"
-                    name="radioFigure"
-                    value={Figure.Figure1}
-                    checked={figure === Figure.Figure1}
-                    onChange={() => {setFigure(Figure.Figure1)}}
-                />
-            </div>
-
-            {/* FIGURE 2 */}
-            <div className="figure">
-                <label>Figure 2</label>
-                <img src="/images/figure2.png" alt="Figure 2" />
-                <input type="radio"
-                    name="radioFigure"
-                    value={Figure.Figure2}
-                    checked={figure === Figure.Figure2}
-                    onChange={() => setFigure(Figure.Figure2)}
-                />
-            </div>
-
-            {/* FIGURE 3 */}
-            <div className="figure">
-                <label>Figure 3</label>
-                <img src="/images/figure3.png" alt="Figure 3" />
-                <input type="radio"
-                    name="radioFigure"
-                    value={Figure.Figure3}
-                    checked={figure === Figure.Figure3}
-                    onChange={() => setFigure(Figure.Figure3)}
-                />
-            </div>
-
-            {/* FIGURE 4 */}
-            <div className="figure">
-                <label>Figure 4</label>
-                <img src="/images/figure4.png" alt="Figure 4" />
-                <input type="radio"
-                    name="radioFigure"
-                    value={Figure.Figure4}
-                    checked={figure === Figure.Figure4}
-                    onChange={() => setFigure(Figure.Figure4)}
-                />
-            </div>
+            <SyllogismFigureItem figure={Figure.Figure1} checked={figure === Figure.Figure1} onChecked={setFigure}/>
+            <SyllogismFigureItem figure={Figure.Figure2} checked={figure === Figure.Figure2} onChecked={setFigure}/>
+            <SyllogismFigureItem figure={Figure.Figure3} checked={figure === Figure.Figure3} onChecked={setFigure}/>
+            <SyllogismFigureItem figure={Figure.Figure4} checked={figure === Figure.Figure4} onChecked={setFigure}/>
         </div>
     )
 }
 
 export default SyllogismTerms
+
+
+interface SyllogismFigureItemProps {
+    figure: Figure;
+    checked: boolean
+    onChecked: (f: Figure) => void;
+}
+
+function SyllogismFigureItem({figure, checked, onChecked}: SyllogismFigureItemProps) {
+    const checkFigureItem = () => onChecked(figure);
+
+    const classNames = "figure" + (checked ? " figure--selected" : "");
+
+    return <div className={classNames} onClick={checkFigureItem}>
+        <label>Figure {figure + 1}</label>
+        <div>
+            <div>{figure === Figure.Figure2 || figure === Figure.Figure4 ? "P    →    M" : "M    →    P"}</div>
+            <div>{figure === Figure.Figure1 || figure === Figure.Figure2 ? "S    →    M" : "M    →    S"}</div>
+            <div>S    →    P</div>
+        </div>
+        <input type="radio"
+               name="radioFigure"
+               value={figure}
+               checked={checked}
+               onChange={checkFigureItem}
+        />
+    </div>
+}
